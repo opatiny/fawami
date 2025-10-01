0ca0f8bee0a8426ef162e835b4693f0a9d7242ba
Oceane Patiny <oceane@patiny.com>
2025-10-01 12:17:16 +0200

    wip: load svg

--- demo/loadSvg.ts
+++ demo/loadSvg.ts
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prefer-add-event-listener */
 import { readFile, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
@@ -7,11 +8,14 @@ import { Canvas, Image } from 'skia-canvas';
 
 import { parseDimension } from './parseDimension.ts';
 
-const filePath = '../data/freesewing-aaron.svg';
+const aaron = '../data/freesewing-aaron-simplified.svg';
+
+const rectangles = '../data/rectangles.svg';
 
 // read the svg as text
-const svg = await readFile(join(import.meta.dirname, filePath), 'utf8');
+const svg = await readFile(join(import.meta.dirname, aaron), 'utf8');
 
+// transform to a DOM
 const dom = new JSDOM(svg, { contentType: 'image/svg+xml' });
 
 console.log(dom.window.document.querySelectorAll('svg').length);
@@ -33,13 +37,18 @@ const heightPx = parseDimension(height, dpcm);
 
 console.log({ widthPx, heightPx });
 
-const pathElements = dom.window.document.querySelectorAll('path');
+const pathElements = dom.window.document.querySelectorAll(
+  'path, rect, circle, ellipse, polygon, line',
+);
 
 console.log(pathElements.length);
 
 for (const pathElement of pathElements) {
-  pathElement.setAttribute('fill', '#ff000');
   pathElement.removeAttribute('class');
+  pathElement.removeAttribute('style');
+  pathElement.setAttribute('fill', '#fff000');
+  pathElement.setAttribute('stroke', '#ffffff');
+  pathElement.setAttribute('stroke-width', '1');
 }
 
 const blackSVG = dom.serialize();
@@ -47,37 +56,30 @@ const blackSVG = dom.serialize();
 await writeFile(join(import.meta.dirname, 'test-black.svg'), blackSVG);
 
 // width and height should be in px
-const scale = 5;
+const scale = 1;
 const canvas = new Canvas(widthPx * scale, heightPx * scale);
+
 const ctx = canvas.getContext('2d');
 
 const img = new Image();
-img.addListener('load', () => {
-  ctx.drawImage(
-    img,
-    0,
-    0,
-    widthPx,
-    heightPx,
-    0,
-    0,
-    widthPx * scale,
-    heightPx * scale,
-  );
-});
-img.src = `data:image/svg+xml;utf8,${encodeURIComponent(blackSVG)}`;
+img.onload = function () {
+  ctx.drawImage(img, 0, 0, width, height, 0, 0, width * 2, height * 2);
+};
+console.log(blackSVG);
+
+img.src = `data:image/svg+xml;utf8,${encodeURIComponent(blackSVG.replaceAll('mm', ''))}`;
+
+await writeFile(
+  join(import.meta.dirname, 'canvas.png'),
+  await canvas.toBuffer('png'),
+);
 
 const ijs = await readCanvas(canvas);
 console.log(ijs);
-write('test-black.png', ijs);
+write(join(import.meta.dirname, 'test-black.png'), ijs);
 
 const mask = Mask.createFrom(ijs);
 console.log(mask);
 
 // save mask
-write('mask.png', mask);
-
-// await writeFile(
-//   join(import.meta.dirname, 'test-black.png'),
-//   await canvas.toBuffer('png'),
-// );
+write(join(import.meta.dirname, 'mask.png'), mask);
---
+++ docs/libDocs.md
@@ -0,0 +1,5 @@
+# Links to the official documentation of the libraries used
+
+- `skia`: https://skia-canvas.org/api/canvas#tobuffer
+- `jsdom`: https://developer.mozilla.org/en-US/docs/Web/API/Document
+- `image-js`: https://api.image-js.org/classes/index.Mask.html
