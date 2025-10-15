47910950804325fff9a3846223e33ca53059dc00
Oceane Patiny <oceane@patiny.com>
2025-10-15 10:20:41 +0200

    feat: implement getIntersectionMatrix

---
+++ src/utils/__tests__/getIntersectionMatrix.test.ts
@@ -0,0 +1,44 @@
+import { Matrix } from 'ml-matrix';
+import { expect, test } from 'vitest';
+
+import { PatternPiece } from '../../PatternPiece.ts';
+import { getIntersectionMatrix } from '../getIntersectionMatrix.ts';
+
+const mask1 = testUtils.createMask([
+  [1, 1, 1],
+  [1, 1, 1],
+  [1, 1, 1],
+]);
+
+const mask2 = testUtils.createMask([
+  [1, 1, 1],
+  [1, 0, 0],
+  [1, 0, 0],
+]);
+
+const mask3 = testUtils.createMask([
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 1],
+]);
+
+const masks = [mask1, mask2, mask3];
+
+const pieces: PatternPiece[] = [];
+
+for (const mask of masks) {
+  const piece = new PatternPiece(mask);
+  pieces.push(piece);
+}
+
+test('all origins (0,0)', async () => {
+  const result = getIntersectionMatrix(pieces);
+
+  const expected = new Matrix([
+    [0, 5, 1],
+    [5, 0, 0],
+    [1, 0, 0],
+  ]);
+
+  expect(result).toStrictEqual(expected);
+});
---
+++ src/utils/getIntersectionMatrix.ts
@@ -0,0 +1,28 @@
+import { Matrix } from 'ml-matrix';
+
+import type { PatternPiece } from '../PatternPiece.ts';
+
+import { getIntersection } from './getIntersection.ts';
+
+/**
+ * Compute the intersection of all pattern pieces in pixels and return it as a matrix.
+ * The intersection of a piece with itself is considered 0.
+ * @param pieces - Array of pattern pieces
+ * @returns The intersection matrix
+ */
+export function getIntersectionMatrix(pieces: PatternPiece[]): Matrix {
+  const n = pieces.length;
+  const matrix = Matrix.zeros(n, n);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      const piece1 = pieces[i] as PatternPiece;
+      const piece2 = pieces[j] as PatternPiece;
+      const intersection = getIntersection(piece1, piece2);
+      matrix.set(i, j, intersection);
+      matrix.set(j, i, intersection);
+    }
+  }
+
+  return matrix;
+}
