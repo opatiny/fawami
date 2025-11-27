c250bf820229ed6c6fccdec8098283ba7036b436
Oceane Patiny <oceane@patiny.com>
2025-11-27 16:48:19 +0100

    chore: do not consider console as error

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,13 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import ts from 'eslint-config-cheminfo-typescript';
 
-export default defineConfig(globalIgnores(['coverage', 'lib']), ts);
+const mergedConfig = {
+	...(ts || {}),
+	rules: {
+		...((ts && ts.rules) || {}),
+		// allow console.* (e.g. console.log) in this project
+		'no-console': 'off',
+	},
+};
+
+export default defineConfig(globalIgnores(['coverage', 'lib']), mergedConfig);
